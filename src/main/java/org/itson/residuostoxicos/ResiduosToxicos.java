a34b50422ebdc972bedf49974ed66fd0be423f32
DANIELPC\Usuario <daniel.pena229185@potros.itson.edu.mx>
2023-02-23 22:18:53 -0700

    Inicio del proyecto, proyecto de java con maven con la versión JDK 19

---
+++ src/main/java/org/itson/residuostoxicos/ResiduosToxicos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.residuostoxicos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Daniel Armando Peña Garcia ID:229185
+ */
+public class ResiduosToxicos {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola_Mundo!");
+    }
+}
