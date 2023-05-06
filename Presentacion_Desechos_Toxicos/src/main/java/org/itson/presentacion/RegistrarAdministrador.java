d24424cb4f163cceb651ffab7761f16a76c3414a
Daniel Peña <daniel.pena229185@potros.itson.edu.mx>
2023-05-05 17:53:19 -0700

    Registro terminado

--- Presentacion_Desechos_Toxicos/src/main/java/org/itson/presentacion/RegistrarAdministrador.java
+++ Presentacion_Desechos_Toxicos/src/main/java/org/itson/presentacion/RegistrarAdministrador.java
@@ -6,6 +6,9 @@
 package org.itson.presentacion;
 
 import java.awt.Color;
+import java.util.List;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
@@ -27,6 +30,12 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
      */
     public RegistrarAdministrador() {
         initComponents();
+        iniciarComponentes();
+    }
+
+    private void iniciarComponentes() {
+        this.campoContrasena.setEchoChar('•');
+        this.campoConfirmarContrasena.setEchoChar('•');
     }
 
     /**
@@ -62,6 +71,9 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
         campoConfirmarContrasena = new javax.swing.JPasswordField();
         checkVisibleContrasena = new javax.swing.JCheckBox();
         contrasenaLbl = new javax.swing.JLabel();
+        registrarmeBtn = new javax.swing.JButton();
+        contrasenaValidaLbl = new javax.swing.JLabel();
+        correoValidoLbl = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -208,7 +220,6 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
         campoContrasena.setForeground(Color.GRAY);
         campoContrasena.setText(CONTRASENA_DEFAULT);
         campoContrasena.setBorder(null);
-	campoContrasena.setEchoChar('•');
         campoContrasena.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 campoContrasenaFocusGained(evt);
@@ -225,6 +236,11 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
         panelFondo.add(campoContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 350, 300, -1));
 
         checkVisibleConfirmarContrasena.setText("Visible");
+        checkVisibleConfirmarContrasena.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkVisibleConfirmarContrasenaActionPerformed(evt);
+            }
+        });
         panelFondo.add(checkVisibleConfirmarContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 430, -1, -1));
         panelFondo.add(renglonConfirmarContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 450, 300, 10));
 
@@ -233,7 +249,6 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
         campoConfirmarContrasena.setForeground(Color.GRAY);
         campoConfirmarContrasena.setText(CONTRASENA_DEFAULT);
         campoConfirmarContrasena.setBorder(null);
-	campoConfirmarContrasena.setEchoChar('•');
         campoConfirmarContrasena.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 campoConfirmarContrasenaFocusGained(evt);
@@ -250,12 +265,30 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
         panelFondo.add(campoConfirmarContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 430, 300, -1));
 
         checkVisibleContrasena.setText("Visible");
+        checkVisibleContrasena.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkVisibleContrasenaActionPerformed(evt);
+            }
+        });
         panelFondo.add(checkVisibleContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 350, -1, -1));
 
         contrasenaLbl.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         contrasenaLbl.setForeground(new java.awt.Color(0, 0, 0));
         contrasenaLbl.setText("Contraseña:");
         panelFondo.add(contrasenaLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 310, -1, -1));
+
+        registrarmeBtn.setBackground(new java.awt.Color(0, 0, 0));
+        registrarmeBtn.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        registrarmeBtn.setForeground(new java.awt.Color(255, 255, 255));
+        registrarmeBtn.setText("Registrarme");
+        registrarmeBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                registrarmeBtnActionPerformed(evt);
+            }
+        });
+        panelFondo.add(registrarmeBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 500, -1, -1));
+        panelFondo.add(contrasenaValidaLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 460, -1, -1));
+        panelFondo.add(correoValidoLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 290, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -272,132 +305,203 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void campoNombresFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoNombresFocusGained
-        if(validarCampoTextoVacio(campoNombres, NOMBRE_DEFAULT)){
+        if (validarCampoTextoVacio(campoNombres, NOMBRE_DEFAULT)) {
             this.campoNombres.setForeground(Color.BLACK);
             this.campoNombres.setText("");
         }
     }//GEN-LAST:event_campoNombresFocusGained
 
     private void campoNombresFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoNombresFocusLost
-        if(validarCampoTextoVacio(campoNombres, NOMBRE_DEFAULT)){
+        if (validarCampoTextoVacio(campoNombres, NOMBRE_DEFAULT)) {
             this.campoNombres.setForeground(Color.GRAY);
             this.campoNombres.setText(NOMBRE_DEFAULT);
         }
     }//GEN-LAST:event_campoNombresFocusLost
 
     private void campoNombresMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_campoNombresMouseClicked
-        if(validarCampoTextoVacio(campoNombres, NOMBRE_DEFAULT)){
+        if (validarCampoTextoVacio(campoNombres, NOMBRE_DEFAULT)) {
             this.campoNombres.setForeground(Color.BLACK);
             this.campoNombres.setText("");
         }
     }//GEN-LAST:event_campoNombresMouseClicked
 
     private void campoPaternoFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoPaternoFocusGained
-        if(validarCampoTextoVacio(campoPaterno, APELLIDO_PATERNO_DEFAULT)){
+        if (validarCampoTextoVacio(campoPaterno, APELLIDO_PATERNO_DEFAULT)) {
             this.campoPaterno.setForeground(Color.BLACK);
             this.campoPaterno.setText("");
         }
     }//GEN-LAST:event_campoPaternoFocusGained
 
     private void campoPaternoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_campoPaternoMouseClicked
-        if(validarCampoTextoVacio(campoPaterno, APELLIDO_PATERNO_DEFAULT)){
+        if (validarCampoTextoVacio(campoPaterno, APELLIDO_PATERNO_DEFAULT)) {
             this.campoPaterno.setForeground(Color.BLACK);
             this.campoPaterno.setText("");
         }
     }//GEN-LAST:event_campoPaternoMouseClicked
 
     private void campoPaternoFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoPaternoFocusLost
-        if(validarCampoTextoVacio(campoPaterno, APELLIDO_PATERNO_DEFAULT)){
+        if (validarCampoTextoVacio(campoPaterno, APELLIDO_PATERNO_DEFAULT)) {
             this.campoPaterno.setForeground(Color.GRAY);
             this.campoPaterno.setText(APELLIDO_PATERNO_DEFAULT);
         }
     }//GEN-LAST:event_campoPaternoFocusLost
 
     private void campoMaternoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_campoMaternoMouseClicked
-        if(validarCampoTextoVacio(campoMaterno, APELLIDO_MATERNO_DEFAULT)){
+        if (validarCampoTextoVacio(campoMaterno, APELLIDO_MATERNO_DEFAULT)) {
             this.campoMaterno.setForeground(Color.BLACK);
             this.campoMaterno.setText("");
         }
     }//GEN-LAST:event_campoMaternoMouseClicked
 
     private void campoMaternoFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoMaternoFocusGained
-        if(validarCampoTextoVacio(campoMaterno, APELLIDO_MATERNO_DEFAULT)){
+        if (validarCampoTextoVacio(campoMaterno, APELLIDO_MATERNO_DEFAULT)) {
             this.campoMaterno.setForeground(Color.BLACK);
             this.campoMaterno.setText("");
         }
     }//GEN-LAST:event_campoMaternoFocusGained
 
     private void campoMaternoFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoMaternoFocusLost
-        if(validarCampoTextoVacio(campoMaterno, APELLIDO_MATERNO_DEFAULT)){
+        if (validarCampoTextoVacio(campoMaterno, APELLIDO_MATERNO_DEFAULT)) {
             this.campoMaterno.setForeground(Color.GRAY);
             this.campoMaterno.setText(APELLIDO_MATERNO_DEFAULT);
         }
     }//GEN-LAST:event_campoMaternoFocusLost
 
     private void campoCorreoFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoCorreoFocusGained
-        if(validarCampoTextoVacio(campoCorreo, CORREO_DEFAULT)){
+        if (validarCampoTextoVacio(campoCorreo, CORREO_DEFAULT)) {
             this.campoCorreo.setForeground(Color.BLACK);
             this.campoCorreo.setText("");
         }
     }//GEN-LAST:event_campoCorreoFocusGained
 
     private void campoCorreoFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoCorreoFocusLost
-        if(validarCampoTextoVacio(campoCorreo, CORREO_DEFAULT)){
+        if (validarCampoTextoVacio(campoCorreo, CORREO_DEFAULT)) {
             this.campoCorreo.setForeground(Color.GRAY);
             this.campoCorreo.setText(CORREO_DEFAULT);
         }
+        this.validarCorreoMostrar();
     }//GEN-LAST:event_campoCorreoFocusLost
 
     private void campoCorreoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_campoCorreoMouseClicked
-        if(validarCampoTextoVacio(campoCorreo, CORREO_DEFAULT)){
+        if (validarCampoTextoVacio(campoCorreo, CORREO_DEFAULT)) {
             this.campoCorreo.setForeground(Color.BLACK);
             this.campoCorreo.setText("");
         }
     }//GEN-LAST:event_campoCorreoMouseClicked
 
     private void campoContrasenaFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoContrasenaFocusGained
-        if(validarCampoTextoVacio(campoContrasena, CONTRASENA_DEFAULT)){
+        if (validarCampoTextoVacio(campoContrasena, CONTRASENA_DEFAULT)) {
             this.campoContrasena.setForeground(Color.BLACK);
             this.campoContrasena.setText("");
         }
     }//GEN-LAST:event_campoContrasenaFocusGained
 
     private void campoContrasenaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_campoContrasenaMouseClicked
-        if(validarCampoTextoVacio(campoContrasena, CONTRASENA_DEFAULT)){
+        if (validarCampoTextoVacio(campoContrasena, CONTRASENA_DEFAULT)) {
             this.campoContrasena.setForeground(Color.BLACK);
             this.campoContrasena.setText("");
         }
     }//GEN-LAST:event_campoContrasenaMouseClicked
 
     private void campoContrasenaFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoContrasenaFocusLost
-        if(validarCampoTextoVacio(campoContrasena, CONTRASENA_DEFAULT)){
+        if (validarCampoTextoVacio(campoContrasena, CONTRASENA_DEFAULT)) {
             this.campoContrasena.setForeground(Color.GRAY);
             this.campoContrasena.setText(CONTRASENA_DEFAULT);
         }
     }//GEN-LAST:event_campoContrasenaFocusLost
 
     private void campoConfirmarContrasenaFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoConfirmarContrasenaFocusGained
-        if(validarCampoTextoVacio(campoConfirmarContrasena, CONTRASENA_DEFAULT)){
+        if (validarCampoTextoVacio(campoConfirmarContrasena, CONTRASENA_DEFAULT)) {
             this.campoConfirmarContrasena.setForeground(Color.BLACK);
             this.campoConfirmarContrasena.setText("");
         }
     }//GEN-LAST:event_campoConfirmarContrasenaFocusGained
 
     private void campoConfirmarContrasenaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_campoConfirmarContrasenaMouseClicked
-        if(validarCampoTextoVacio(campoConfirmarContrasena, CONTRASENA_DEFAULT)){
+        if (validarCampoTextoVacio(campoConfirmarContrasena, CONTRASENA_DEFAULT)) {
             this.campoConfirmarContrasena.setForeground(Color.BLACK);
             this.campoConfirmarContrasena.setText("");
         }
     }//GEN-LAST:event_campoConfirmarContrasenaMouseClicked
 
     private void campoConfirmarContrasenaFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoConfirmarContrasenaFocusLost
-        if(validarCampoTextoVacio(campoConfirmarContrasena, CONTRASENA_DEFAULT)){
+        if (validarCampoTextoVacio(campoConfirmarContrasena, CONTRASENA_DEFAULT)) {
             this.campoConfirmarContrasena.setForeground(Color.GRAY);
             this.campoConfirmarContrasena.setText(CONTRASENA_DEFAULT);
         }
+        this.validarContrasenaMostrar();
     }//GEN-LAST:event_campoConfirmarContrasenaFocusLost
 
-    
+    private void checkVisibleContrasenaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkVisibleContrasenaActionPerformed
+        if (this.checkVisibleContrasena.isSelected()) {
+            this.campoContrasena.setEchoChar((char) 0);
+        } else {
+            this.campoContrasena.setEchoChar('•');
+        }
+    }//GEN-LAST:event_checkVisibleContrasenaActionPerformed
+
+    private void checkVisibleConfirmarContrasenaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkVisibleConfirmarContrasenaActionPerformed
+        if (this.checkVisibleConfirmarContrasena.isSelected()) {
+            this.campoConfirmarContrasena.setEchoChar((char) 0);
+        } else {
+            this.campoConfirmarContrasena.setEchoChar('•');
+        }
+    }//GEN-LAST:event_checkVisibleConfirmarContrasenaActionPerformed
+
+    private void registrarmeBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registrarmeBtnActionPerformed
+        int i = 0;
+        if (validarContrasenasVacio()) {
+            if (!validarContrasenaMostrar()) {
+                JOptionPane.showMessageDialog(this, "Contraseñas no coinciden", "No puede registrarse", JOptionPane.ERROR_MESSAGE);
+                i++;
+            }
+        } else {
+
+            List<String> camposVacios = this.validarCamposVacios();
+            if (!camposVacios.isEmpty()) {
+                String mensaje = "Los siguientes campos se encuentran vacíos \n";
+                for (String campos : camposVacios) {
+                    mensaje += campos + "\n";
+                }
+                JOptionPane.showMessageDialog(this, mensaje, "No puedes registrarte", JOptionPane.ERROR_MESSAGE);
+                i++;
+            }
+        }
+        if (i == 0) {
+            //Método registrar
+            JOptionPane.showMessageDialog(this, "Registro exitoso", "Nuevo Registro", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }//GEN-LAST:event_registrarmeBtnActionPerformed
+
+    private boolean validarContrasenas() {
+        String contrasena = this.campoContrasena.getText();
+        String confirmarContrasena = this.campoConfirmarContrasena.getText();
+        return contrasena.equals(confirmarContrasena);
+    }
+
+    private List<String> validarCamposVacios() {
+        List<String> campos = new LinkedList<>();
+        if (this.validarCampoTextoVacio(campoNombres, NOMBRE_DEFAULT)) {
+            campos.add("- Nombre");
+        }
+        if (this.validarCampoTextoVacio(campoPaterno, APELLIDO_PATERNO_DEFAULT)) {
+            campos.add("- Apellido Paterno");
+        }
+        if (this.validarCampoTextoVacio(campoMaterno, APELLIDO_MATERNO_DEFAULT)) {
+            campos.add("- Apellido Materno");
+        }
+        if (this.validarCampoTextoVacio(campoCorreo, CORREO_DEFAULT)) {
+            campos.add("- Correo");
+        }
+        if (this.campoContrasena.getText().isEmpty() || this.campoContrasena.getText().equals(CONTRASENA_DEFAULT)) {
+            campos.add("- Contraseña");
+        }
+        if (this.campoConfirmarContrasena.getText().isEmpty() || this.campoConfirmarContrasena.getText().equals(CONTRASENA_DEFAULT)) {
+            campos.add("- Confirmar contraseña");
+        }
+        return campos;
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -437,6 +541,57 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
         return campoTexto.getText().isEmpty() || campoTexto.getText().equals(textoDefault);
     }
 
+    private boolean validarContrasenasVacio() {
+        if (this.campoContrasena.getText().isEmpty() || this.campoContrasena.getText().equals(CONTRASENA_DEFAULT)) {
+            return false;
+        }
+        if (this.campoConfirmarContrasena.getText().isEmpty() || this.campoConfirmarContrasena.getText().equals(CONTRASENA_DEFAULT)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validarContrasenaMostrar() {
+        if (validarContrasenasVacio()) {
+            if (validarContrasenas()) {
+                this.contrasenaValidaLbl.setForeground(Color.GREEN);
+                this.contrasenaValidaLbl.setText("Contraseñas coinciden");
+                return true;
+            } else {
+                this.contrasenaValidaLbl.setForeground(Color.red);
+                this.contrasenaValidaLbl.setText("Contraseñas no coinciden");
+            }
+        } else {
+            this.contrasenaValidaLbl.setForeground(Color.red);
+            this.contrasenaValidaLbl.setText("Contraseñas no coinciden");
+        }
+        return false;
+    }
+
+    public static boolean validarCorreo(String correo) {
+        String expresionRegular = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        return correo.matches(expresionRegular);
+    }
+
+    private boolean validarCorreoMostrar() {
+    String correo = campoCorreo.getText();
+
+    if (correo.isEmpty()) {
+        correoValidoLbl.setForeground(Color.RED);
+        correoValidoLbl.setText("El campo correo está vacío");
+        return false;
+    } else if (validarCorreo(correo)) {
+        correoValidoLbl.setForeground(Color.GREEN);
+        correoValidoLbl.setText("El correo es válido");
+        return true;
+    } else {
+        correoValidoLbl.setForeground(Color.RED);
+        correoValidoLbl.setText("El correo no es válido");
+        return false;
+    }
+}
+
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel apellidosLbl;
     private javax.swing.JPasswordField campoConfirmarContrasena;
@@ -448,13 +603,16 @@ public class RegistrarAdministrador extends javax.swing.JFrame {
     private javax.swing.JCheckBox checkVisibleConfirmarContrasena;
     private javax.swing.JCheckBox checkVisibleContrasena;
     private javax.swing.JLabel contrasenaLbl;
+    private javax.swing.JLabel contrasenaValidaLbl;
     private javax.swing.JLabel correoLbl;
+    private javax.swing.JLabel correoValidoLbl;
     private javax.swing.JLabel lblConfirmarContrasenaLbl;
     private javax.swing.JLabel maternoLbl;
     private javax.swing.JLabel nombresLbl;
     private javax.swing.JPanel panelFondo;
     private javax.swing.JPanel panelInfo;
     private javax.swing.JLabel paternoLbl;
+    private javax.swing.JButton registrarmeBtn;
     private javax.swing.JLabel registrarmeLbl;
     private javax.swing.JSeparator renglonConfirmarContrasena;
     private javax.swing.JSeparator renglonContrasena;
