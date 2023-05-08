b7ba47758215b7a1d8fe2668dea51d78f3bdfa4a
DESKTOP-KAHS6MH\Dapgp <daniel.pena229185@potros.itson.edu.mx>
2023-05-07 18:36:35 -0700

    Proyecto Negocio Creado

---
+++ Residuos_Toxicos_Negocio/src/main/java/org/itson/Pruebas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson;
+
+/**
+ *
+ * @author Daniel Armando Peña Garcia ID:229185
+ */
+public class Pruebas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
