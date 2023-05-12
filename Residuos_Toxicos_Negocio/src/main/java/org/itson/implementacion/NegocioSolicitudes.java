ec47509379a8701bd8f4b3694f69a1547afc8841
OscarMinjarez <oscarminjarez@outlook.com>
2023-05-12 12:46:26 -0700

    Se hizo la validación de cantidad de traslados por día.

--- Residuos_Toxicos_Negocio/src/main/java/org/itson/implementacion/NegocioSolicitudes.java
+++ Residuos_Toxicos_Negocio/src/main/java/org/itson/implementacion/NegocioSolicitudes.java
@@ -6,19 +6,31 @@ package org.itson.implementacion;
 
 import com.dominio.MedidaResiduo;
 import com.dominio.Residuo;
+import com.dominio.Solicitud;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import org.itson.DTO.SolicitudDTO;
+import org.itson.excepciones.PersistenciaException;
 import org.itson.excepciones.ValidacionExcepcion;
+import org.itson.interfaces.ISolicitudesDAO;
+import org.itson.implementaciones.bd.DAOFactory;
 
 /**
  *
  * @author oscar
  */
 public class NegocioSolicitudes {
+    
+    ISolicitudesDAO solicitudesDAO;
+    
+    public NegocioSolicitudes() {
+        this.solicitudesDAO = DAOFactory.getSolicitudesDAO();
+    }
 
     private Boolean validarFechaParaTrasladoDeResiduos(Date fecha) throws ValidacionExcepcion {
         if (!fecha.after(new Date())) {
-            throw new ValidacionExcepcion("");
+            throw new ValidacionExcepcion("La fecha seleccionada no debe ser anterior a la fecha actual.");
         }
         
         return true;
@@ -26,7 +38,7 @@ public class NegocioSolicitudes {
     
     private Boolean validaQueTengaAlmenosUnResiduo(List<Residuo> residuos) throws ValidacionExcepcion {
         if (residuos.size() <= 0) {
-            throw new ValidacionExcepcion("No se recibió ningún residuo.");
+            throw new ValidacionExcepcion("No hay residuos.");
         }
         
         return true;
@@ -35,7 +47,7 @@ public class NegocioSolicitudes {
     private Boolean validaLaUnidadDeMedida(Residuo residuo) throws ValidacionExcepcion {
         for (MedidaResiduo m : MedidaResiduo.values()) {
             if (!m.name().equals(residuo.getMedida_residuo())) {
-                throw new ValidacionExcepcion("El tipo de unidad de medida no es válida.");
+                throw new ValidacionExcepcion("Unidad de medida inválida.");
             }
         }
         
@@ -43,6 +55,20 @@ public class NegocioSolicitudes {
     }
     
     private Boolean validarNoMasDeCincoTrasladosPorDia(Date fecha) throws ValidacionExcepcion {
-        return null;
+        SolicitudDTO filtros = new SolicitudDTO();
+        filtros.setFechaSolicitada(fecha);
+        List<Solicitud> solicitudes = new ArrayList<>();
+        
+        try {
+            solicitudes = this.solicitudesDAO.consultar(filtros);
+        } catch (PersistenciaException e) {
+            throw new ValidacionExcepcion("No se encontró ninguna solicitud.", e.getCause());
+        }
+        
+        if (solicitudes.size() >= 5) {
+            throw new ValidacionExcepcion("Ya hay cinco traslados registrados en esta fecha.");
+        }
+        
+        return true;
     }
 }
