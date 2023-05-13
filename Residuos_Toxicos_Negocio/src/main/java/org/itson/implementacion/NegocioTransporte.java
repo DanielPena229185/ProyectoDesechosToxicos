ab4bb584cb12b8811452e146946086d71cce5ad7
DESKTOP-KAHS6MH\Dapgp <daniel.pena229185@potros.itson.edu.mx>
2023-05-13 01:40:16 -0700

    Casi se termina las validaciones de Transporte

---
+++ Residuos_Toxicos_Negocio/src/main/java/org/itson/implementacion/NegocioTransporte.java
@@ -0,0 +1,84 @@
+/**
+ * NegocioTransporte.java
+ * 12 may. 2023 22:32:52
+ */
+package org.itson.implementacion;
+
+import com.dominio.Transporte;
+import com.dominio.Vehiculo;
+import java.util.LinkedList;
+import java.util.List;
+import org.itson.excepciones.NegocioException;
+import org.itson.excepciones.ValidacionException;
+import org.itson.implementaciones.fachada.FachadaPersistencia;
+import org.itson.interfaces.INegocioTransporte;
+import org.itson.interfaces.IPersistencia;
+
+/**
+ * Descripción de la clase:
+ *
+ * @author Daniel Armando Peña Garcia ID:229185
+ */
+public class NegocioTransporte implements INegocioTransporte {
+
+    IPersistencia persistencia;
+
+    /**
+     * Constructor por default
+     */
+    public NegocioTransporte() {
+        persistencia = new FachadaPersistencia();
+    }
+
+    @Override
+    public Transporte insertarTransporte(Transporte transporte) throws NegocioException {
+        return null;
+    }
+
+    private Transporte validarTransporte(Transporte transporte) throws ValidacionException {
+
+        List<String> camposError = new LinkedList<>();
+
+        if (transporte == null) {
+            throw new ValidacionException("- No hay información del transporte");
+        }
+
+        //Valida que almenos exista un vehículo seleccionado
+        List<Vehiculo> vehiculos = transporte.getVehiculos();
+        
+        if(validarListaVacia(vehiculos)){
+            camposError.add("- No hay vehículos seleccionados");
+        }
+        
+        //Validar kilometros
+        Float kilometros = transporte.getKilometros();
+        if(kilometros == null || kilometros == 0){
+            camposError.add("- No se especificó los kilometros");
+        }
+        
+        //Validar costo
+        Float costo = transporte.getCoste();
+        if(costo == null || costo == 0){
+            camposError.add("- No se especificó el costo");
+        }
+        return null;
+        
+    }
+
+    /**
+     * Válida que una lista esté vacia, si la lista está vacía, retorna true, en
+     * caso contrarío, regresa false
+     *
+     * @param lista Lista que se quiere validar
+     * @return True si la lista está vacía, false en caso contrario
+     */
+    private boolean validarListaVacia(List lista) {
+        if (lista == null || lista.isEmpty()) {
+            //Lista vacía o nula
+            return true;
+        }
+        //Lista no está vacía
+        return false;
+    }
+
+}
--- Residuos_Toxicos_Negocio/src/main/java/org/itson/interfaces/INegocioTransporte.java
+++ Residuos_Toxicos_Negocio/src/main/java/org/itson/interfaces/INegocioTransporte.java
@@ -1,15 +1,19 @@
 /**
-* INegocioTransporte.java
-* 12 may. 2023 16:28:15
-*/ 
-
+ * INegocioTransporte.java
+ * 12 may. 2023 16:28:15
+ */
 package org.itson.interfaces;
 
+import com.dominio.Transporte;
+import org.itson.excepciones.NegocioException;
+
 /**
- * Descripción de la interface: 
+ * Descripción de la interface:
  *
  * @author Daniel Armando Peña Garcia ID:229185
  */
 public interface INegocioTransporte {
 
+    Transporte insertarTransporte(Transporte transporte) throws NegocioException;
+    
 }
